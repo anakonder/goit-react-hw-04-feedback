39645b0454668e4c104b8f588c61e5a2564bcf2d
anakonder <120507138+anakonder@users.noreply.github.com>
2023-07-26 08:53:10 +0300

    working-code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,75 @@
-import { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "components/FeedbackOptions/FeedbackOptions";
 import Statistics from "components/Statistics/Statistics";
 import Section from "components/Section/Section";
 import Notification from "components/Notification/Notification"
 import PropTypes from "prop-types"
 import "./GeneralStyles.css"
-// import StateComponent from "./StateComponent/StateComponent"
 
-export class App extends Component {
+
+
+export function App({title}) {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
   
-  state = {
-            good: 0,
-            neutral: 0,
-            bad: 0    
+  const countTotalFeedback = () => {
+    let total = good + neutral + bad
+    return total 
   }
   
-  countTotalFeedback() {
-        let total = this.state.good + this.state.neutral + this.state.bad
-        return total 
+  const countPositiveFeedbackPercentage = () => {
+    return Math.floor(good / countTotalFeedback() * 100) || 0;
   }
   
-  countPositiveFeedbackPercentage() {
-    return Math.floor(this.state.good / this.countTotalFeedback() * 100) || 0;
+  const handleFeedback = (option) => {
+    switch (option) {
+      case "good":
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case "neutral":
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case "bad":
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        break;
     }
-
-  handleFeedback = (option) => {
-        this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
-    }));
-  }
-
-  render() {
-        const options = Object.keys(this.state);
-        const {title} = this.props
-        const {good, neutral, bad} = this.state
-        return (
-            <div className="cardWrap">
+  };
+    
+  
+  return (
+      <div className="cardWrap">
                 <Section
                     title={title}
-                />
+                    />
                 <FeedbackOptions
-                    options={options}
-                    onLeaveFeedback={this.handleFeedback}
-                />
-                {this.countTotalFeedback() !== 0
+                    options={["good", "neutral", "bad"]}
+                    onLeaveFeedback={handleFeedback}
+                    />
+                {countTotalFeedback() !== 0
                     ?
                     <Statistics
-                        good={good}
-                        neutral={neutral}
-                        bad={bad}
-                        total={this.countTotalFeedback()}
-                        positivePercentage={this.countPositiveFeedbackPercentage()}
+                    good={good}
+                    neutral={neutral}
+                    bad={bad}
+                    total={countTotalFeedback()}
+                    positivePercentage={countPositiveFeedbackPercentage()}
                     />
                     :
-                    <Notification message="There is no feedback" />}
-                              
+                    <Notification message="There is no feedback" />}                             
             </div>
         )
     }
-  
-
-  
-};
-
-
-App.propTypes = {
+    
+      
+      
+      
+      
+      
+      
+      App.propTypes = {
 title: PropTypes.string.isRequired,
 };
 
